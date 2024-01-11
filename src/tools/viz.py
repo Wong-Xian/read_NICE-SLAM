952ec0f94df3be55bd802a77ecc0e9bf6b1d44fb
wx <wjh15875344277@outlook.com>
2024-01-11 21:33:03 +0800

    read code

--- src/tools/viz.py
+++ src/tools/viz.py
@@ -12,27 +12,29 @@ def normalize(x):
 
 
 def create_camera_actor(i, is_gt=False, scale=0.005):
-    cam_points = scale * np.array([
-        [0,   0,   0],
-        [-1,  -1, 1.5],
-        [1,  -1, 1.5],
-        [1,   1, 1.5],
-        [-1,   1, 1.5],
-        [-0.5, 1, 1.5],
-        [0.5, 1, 1.5],
-        [0, 1.2, 1.5]])
+    cam_points = scale * np.array([[0  ,  0,   0],
+                                   [-1 , -1, 1.5],
+                                   [1  , -1, 1.5],
+                                   [1  ,  1, 1.5],
+                                   [-1 ,  1, 1.5],
+                                   [-0.5, 1, 1.5],
+                                   [0.5,  1, 1.5],
+                                   [0  ,1.2, 1.5]])
 
-    cam_lines = np.array([[1, 2], [2, 3], [3, 4], [4, 1], [1, 3], [2, 4],
-                          [1, 0], [0, 2], [3, 0], [0, 4], [5, 7], [7, 6]])
+    cam_lines = np.array([[1, 2], [2, 3], [3, 4], 
+                          [4, 1], [1, 3], [2, 4],
+                          [1, 0], [0, 2], [3, 0], 
+                          [0, 4], [5, 7], [7, 6]])
+    
     points = []
+    
     for cam_line in cam_lines:
-        begin_points, end_points = cam_points[cam_line[0]
-                                              ], cam_points[cam_line[1]]
+        begin_points, end_points = cam_points[cam_line[0]], cam_points[cam_line[1]]
         t_vals = np.linspace(0., 1., 100)
         begin_points, end_points
-        point = begin_points[None, :] * \
-            (1.-t_vals)[:, None] + end_points[None, :] * (t_vals)[:, None]
+        point = begin_points[None, :] * (1.-t_vals)[:, None] + end_points[None, :] * (t_vals)[:, None]
         points.append(point)
+    
     points = np.concatenate(points)
     color = (0.0, 0.0, 0.0) if is_gt else (1.0, .0, .0)
     camera_actor = o3d.geometry.PointCloud(
@@ -61,31 +63,34 @@ def draw_trajectory(queue, output, init_pose, cam_scale,
         cam = vis.get_view_control().convert_to_pinhole_camera_parameters()
         while True:
             try:
-                data = draw_trajectory.queue.get_nowait()
+                data = draw_trajectory.queue.get_nowait()   # 从队列中取一个 帧？
+                
+                # pose 的情况
                 if data[0] == 'pose':
-                    i, pose, is_gt = data[1:]
+                    i, pose, is_gt = data[1:]   # 获取索引，位姿，是否是gt
                     if is_gt:
                         i += 100000
 
                     if i in draw_trajectory.cameras:
                         cam_actor, pose_prev = draw_trajectory.cameras[i]
-                        pose_change = pose @ np.linalg.inv(pose_prev)
+                        pose_change = pose @ np.linalg.inv(pose_prev)   # 计算当前位姿和上一位姿的变化
 
-                        cam_actor.transform(pose_change)
-                        vis.update_geometry(cam_actor)
+                        cam_actor.transform(pose_change)    # 根据 pose_change 更新 cam_actor
+                        vis.update_geometry(cam_actor)      # 更新场景几何
 
                         if i in draw_trajectory.points:
-                            pc = draw_trajectory.points[i]
-                            pc.transform(pose_change)
-                            vis.update_geometry(pc)
+                            pc = draw_trajectory.points[i]  # 获取点的位置
+                            pc.transform(pose_change)       # 根据 pose_change 更新点的位置
+                            vis.update_geometry(pc)         # 更新点云的场景几何
 
                     else:
                         cam_actor = create_camera_actor(i, is_gt, cam_scale)
-                        cam_actor.transform(pose)
+                        cam_actor.transform(pose)           # 根据 pose_change 更新 cam_actor
                         vis.add_geometry(cam_actor)
 
                     draw_trajectory.cameras[i] = (cam_actor, pose)
 
+                # mesh 的情况
                 elif data[0] == 'mesh':
                     meshfile = data[1]
                     if draw_trajectory.mesh is not None:
@@ -101,6 +106,7 @@ def draw_trajectory(queue, output, init_pose, cam_scale,
                         -np.asarray(draw_trajectory.mesh.triangle_normals))
                     vis.add_geometry(draw_trajectory.mesh)
 
+                # traj 的情况
                 elif data[0] == 'traj':
                     i, is_gt = data[1:]
 
@@ -124,6 +130,7 @@ def draw_trajectory(queue, output, init_pose, cam_scale,
                         draw_trajectory.traj_actor = traj_actor
                         vis.add_geometry(draw_trajectory.traj_actor)
 
+                # reset 的情况
                 elif data[0] == 'reset':
                     draw_trajectory.warmup = -1
 
@@ -152,19 +159,19 @@ def draw_trajectory(queue, output, init_pose, cam_scale,
             vis.capture_screen_image(
                 f'{output}/tmp_rendering/{draw_trajectory.frame_idx:06d}.jpg')
 
-    vis = o3d.visualization.Visualizer()
+    vis = o3d.visualization.Visualizer()    # 实例化一个“可视化界面”
 
-    vis.register_animation_callback(animation_callback)
-    vis.create_window(window_name=output, height=1080, width=1920)
+    vis.register_animation_callback(animation_callback) # 注册一个回调函数
+    vis.create_window(window_name=output, height=1080, width=1920)# 创建窗口
     vis.get_render_option().point_size = 4
     vis.get_render_option().mesh_show_back_face = False
 
-    ctr = vis.get_view_control()
-    ctr.set_constant_z_near(near)
-    ctr.set_constant_z_far(1000)
+    ctr = vis.get_view_control()    # 可以控制视角的实例
+    ctr.set_constant_z_near(near)   # 最近距离？
+    ctr.set_constant_z_far(1000)    # 最远距离？
 
     # set the viewer's pose in the back of the first frame's pose
-    param = ctr.convert_to_pinhole_camera_parameters()
+    param = ctr.convert_to_pinhole_camera_parameters()  # 转换成针孔相机模型参数
     init_pose[:3, 3] += 2*normalize(init_pose[:3, 2])
     init_pose[:3, 2] *= -1
     init_pose[:3, 1] *= -1
@@ -173,30 +180,35 @@ def draw_trajectory(queue, output, init_pose, cam_scale,
     param.extrinsic = init_pose
     ctr.convert_from_pinhole_camera_parameters(param)
 
-    vis.run()
-    vis.destroy_window()
+    vis.run()           # 运行窗口
+    vis.destroy_window()# 销毁窗口
 
 
 class SLAMFrontend:
     def __init__(self, output, init_pose, cam_scale=1, save_rendering=False,
                  near=0, estimate_c2w_list=None, gt_c2w_list=None):
         self.queue = Queue()
-        self.p = Process(target=draw_trajectory, args=(
-            self.queue, output, init_pose, cam_scale, save_rendering,
-            near, estimate_c2w_list, gt_c2w_list))
+        self.p = Process(target=draw_trajectory, 
+                         args=(self.queue, 
+                               output, 
+                               init_pose, 
+                               cam_scale, 
+                               save_rendering,
+                               near, 
+                               estimate_c2w_list, 
+                               gt_c2w_list))
 
     def update_pose(self, index, pose, gt=False):
-        if isinstance(pose, torch.Tensor):
-            pose = pose.cpu().numpy()
-
-        pose[:3, 2] *= -1
-        self.queue.put_nowait(('pose', index, pose, gt))
+        if isinstance(pose, torch.Tensor):  # 如果 pose 是 torch.Tensor 类型
+            pose = pose.cpu().numpy()       # 将其转换成 numpy 类型
+        pose[:3, 2] *= -1   # 将 pose 第三列前三个数取负数
+        self.queue.put_nowait(('pose', index, pose, gt))# 加入队列
         
     def update_mesh(self, path):
-        self.queue.put_nowait(('mesh', path))
+        self.queue.put_nowait(('mesh', path))   # 把 mesh 加入队列
 
     def update_cam_trajectory(self, c2w_list, gt):
-        self.queue.put_nowait(('traj', c2w_list, gt))
+        self.queue.put_nowait(('traj', c2w_list, gt))   # 把姿态和gt值加入队列
 
     def reset(self):
         self.queue.put_nowait(('reset', ))
--- visualizer.py
+++ visualizer.py
@@ -12,57 +12,66 @@ from src import config
 from src.tools.viz import SLAMFrontend
 from src.utils.datasets import get_dataset
 
+# 假设命令为
+# python visualizer.py configs/Apartment/apartment.yaml --output output/vis/Apartment
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
-        description='Arguments to visualize the SLAM process.'
-    )
+    parser = argparse.ArgumentParser(description='Arguments to visualize the SLAM process.')
     parser.add_argument('config', type=str, help='Path to config file.')
+    # config 参数为 'configs/Apartment/apartment.yaml'
     parser.add_argument('--input_folder', type=str,
-                        help='input folder, this have higher priority, can overwrite the one in config file')
+        help='input folder, this have higher priority, can overwrite the one in config file')
     parser.add_argument('--output', type=str,
-                        help='output folder, this have higher priority, can overwrite the one inconfig file')
+        help='output folder, this have higher priority, can overwrite the one in config file')
+    # output 参数为 'output/vis/Apartment'
     nice_parser = parser.add_mutually_exclusive_group(required=False)
     nice_parser.add_argument('--nice', dest='nice', action='store_true')
     nice_parser.add_argument('--imap', dest='nice', action='store_false')
     parser.set_defaults(nice=True)
-    parser.add_argument('--save_rendering',
-                        action='store_true', help='save rendering video to `vis.mp4` in output folder ')
-    parser.add_argument('--vis_input_frame',
-                        action='store_true', help='visualize input frames')
-    parser.add_argument('--no_gt_traj',
-                        action='store_true', help='not visualize gt trajectory')
+    parser.add_argument('--save_rendering', action='store_true',
+                        help='save rendering video to `vis.mp4` in output folder ')
+    parser.add_argument('--vis_input_frame', action='store_true', help='visualize input frames')
+    parser.add_argument('--no_gt_traj', action='store_true', help='not visualize gt trajectory')
     args = parser.parse_args()
-    cfg = config.load_config(
-        args.config, 'configs/nice_slam.yaml' if args.nice else 'configs/imap.yaml')
-    scale = cfg['scale']
-    output = cfg['data']['output'] if args.output is None else args.output
-    if args.vis_input_frame:
-        frame_reader = get_dataset(cfg, args, scale, device='cpu')
-        frame_loader = DataLoader(
-            frame_reader, batch_size=1, shuffle=False, num_workers=4)
-    ckptsdir = f'{output}/ckpts'
-    if os.path.exists(ckptsdir):
-        ckpts = [os.path.join(ckptsdir, f)
-                 for f in sorted(os.listdir(ckptsdir)) if 'tar' in f]
-        if len(ckpts) > 0:
-            ckpt_path = ckpts[-1]
-            print('Get ckpt :', ckpt_path)
-            ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
-            estimate_c2w_list = ckpt['estimate_c2w_list']
-            gt_c2w_list = ckpt['gt_c2w_list']
-            N = ckpt['idx']
-    estimate_c2w_list[:, :3, 3] /= scale
-    gt_c2w_list[:, :3, 3] /= scale
-    estimate_c2w_list = estimate_c2w_list.cpu().numpy()
-    gt_c2w_list = gt_c2w_list.cpu().numpy()
+    cfg = config.load_config(args.config, 'configs/nice_slam.yaml' if args.nice else 'configs/imap.yaml')
+    # 加载 'configs/Apartment/apartment.yaml' 和 'configs/nice_slam.yaml' 两个配置文件
+    
+    scale = cfg['scale']# 获取 scale 参数 在 nice-slam.yaml 文件中为 1
 
+    # 命令行有 output 参数，就使用命令行的路径，否则用 yaml 中的路径；此处 'output/vis/Apartment'
+    output = cfg['data']['output'] if args.output is None else args.output
+    
+    if args.vis_input_frame:    # 命令行参数里有 --vis_input_frame 的情况
+        frame_reader = get_dataset(cfg, args, scale, device='cpu')# 根据 cfg['dataset'] 实例化 azure 类型数据集，其他传入参数未使用
+        frame_loader = DataLoader(frame_reader, batch_size=1, shuffle=False, num_workers=4)# 加载数据
+    ckptsdir = f'{output}/ckpts'    # ckptsdir = output/vis/Apartment/ckpts
+    if os.path.exists(ckptsdir):    # checkpoint 路径存在
+        ckpts = [os.path.join(ckptsdir, f) for f in sorted(os.listdir(ckptsdir)) if 'tar' in f]
+        # 上一条语句产生的效果
+        # ckpts = output/vis/Apartment/ckpts/00500.tar
+        #         output/vis/Apartment/ckpts/01000.tar
+        #         output/vis/Apartment/ckpts/01500.tar
+        #         output/vis/Apartment/ckpts/02000.tar
+        #           ......
+        if len(ckpts) > 0:  # 列表不为空
+            ckpt_path = ckpts[-1]   # 获取最后一个 tar 文件
+            print('Get ckpt :', ckpt_path)
+            ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))# 加载最后一个tar文件
+            estimate_c2w_list = ckpt['estimate_c2w_list']# 获取其中的 estimate_c2w_list 这个列表
+            gt_c2w_list = ckpt['gt_c2w_list']# 获取其中的 gt_c2w_list
+            N = ckpt['idx']# 获取 idx 值
+    estimate_c2w_list[:, :3, 3] /= scale    # 缩放，此处除以1
+    gt_c2w_list[:, :3, 3] /= scale          # 缩放，此处除以1
+    estimate_c2w_list = estimate_c2w_list.cpu().numpy() # 转换成 numpy 数组
+    gt_c2w_list = gt_c2w_list.cpu().numpy()             # 转换成 numpy 数组
+
+    # 实例化类
     frontend = SLAMFrontend(output, init_pose=estimate_c2w_list[0], cam_scale=0.3,
                             save_rendering=args.save_rendering, near=0,
                             estimate_c2w_list=estimate_c2w_list, gt_c2w_list=gt_c2w_list).start()
 
     for i in tqdm(range(0, N+1)):
-        # show every second frame for speed up
+        # 命令行里有 vis_input_frame 参数的情况，每两帧显示一次图片，用于加速
         if args.vis_input_frame and i % 2 == 0:
             idx, gt_color, gt_depth, gt_c2w = frame_reader[i]
             depth_np = gt_depth.numpy()
@@ -77,20 +86,24 @@ if __name__ == '__main__':
             cv2.imshow(f'Input RGB-D Sequence', whole[:, :, ::-1])
             cv2.waitKey(1)
         time.sleep(0.03)
-        meshfile = f'{output}/mesh/{i:05d}_mesh.ply'
-        if os.path.isfile(meshfile):
-            frontend.update_mesh(meshfile)
-        frontend.update_pose(1, estimate_c2w_list[i], gt=False)
+
+        meshfile = f'{output}/mesh/{i:05d}_mesh.ply'    # 生成文件名
+        # meshfile = 'output/vis/Apartment/mesh/xxxxx_mesh.ply'
+        
+        if os.path.isfile(meshfile):    # 如果有这个文件的话
+            frontend.update_mesh(meshfile)  # 将 meshfile 存放入队列
+        
+        frontend.update_pose(1, estimate_c2w_list[i], gt=False) # 更新位姿（放入队列）
+        
         if not args.no_gt_traj:
-            frontend.update_pose(1, gt_c2w_list[i], gt=True)
+            frontend.update_pose(1, gt_c2w_list[i], gt=True)    # 更新位姿（放入队列）
         # the visualizer might get stucked if update every frame
         # with a long sequence (10000+ frames)
-        if i % 10 == 0:
-            frontend.update_cam_trajectory(i, gt=False)
+        if i % 10 == 0: # 每 10 帧更新一次相机轨迹
+            frontend.update_cam_trajectory(i, gt=False)         # 更新相机轨迹（放入队列）
             if not args.no_gt_traj:
-                frontend.update_cam_trajectory(i, gt=True)
+                frontend.update_cam_trajectory(i, gt=True)      # 更新相机轨迹（放入队列）
 
-    if args.save_rendering:
+    if args.save_rendering: # 根据命令行决定是否保存渲染过程的动画
         time.sleep(1)
-        os.system(
-            f"/usr/bin/ffmpeg -f image2 -r 30 -pattern_type glob -i '{output}/tmp_rendering/*.jpg' -y {output}/vis.mp4")
+        os.system(f"/usr/bin/ffmpeg -f image2 -r 30 -pattern_type glob -i '{output}/tmp_rendering/*.jpg' -y {output}/vis.mp4")
